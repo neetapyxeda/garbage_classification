3a2a34e29f7489f014352c48f890fbc5103c89b4
neetapyxeda <neeta.alex@pyxeda.ai>
2025-08-08 09:12:18 +0000

    I have changed requirements

--- app.py
+++ app.py
@@ -71,37 +71,37 @@ classification_model = load_sklearn_models("MLP_Epochs_40_LearningRate_0.0001.pk
 
 
 # web app
+def run_app():
+    # title
+    st.title("Garbage Classification")
+    # image
+    st.image(IMG_ADDRESS, caption = "Garbage Classification")
 
-# title
-st.title("Garbage Classification")
-# image
-st.image(IMG_ADDRESS, caption = "Garbage Classification")
+    # input image
+    st.subheader("Please Upload a Raw Image")
 
-# input image
-st.subheader("Please Upload a Raw Image")
+    # file uploader
+    image = st.file_uploader("Please Upload a Raw Image", type = ["jpg", "png", "jpeg"], accept_multiple_files = False, help = "Upload an Image")
 
-# file uploader
-image = st.file_uploader("Please Upload a Raw Image", type = ["jpg", "png", "jpeg"], accept_multiple_files = False, help = "Upload an Image")
+    if image:
+        user_image = Image.open(image)
+        # save the image to set the path
+        user_image.save(IMAGE_NAME)
+        # set the user image
+        st.image(user_image, caption = "User Uploaded Image")
 
-if image:
-    user_image = Image.open(image)
-    # save the image to set the path
-    user_image.save(IMAGE_NAME)
-    # set the user image
-    st.image(user_image, caption = "User Uploaded Image")
-
-    #get the features
-    with st.spinner("Processing......."):
-        image_features = featurization(IMAGE_NAME, ConvNeXtXLarge_featurized_model)
-        probs = classification_model.predict_proba(image_features)
-        pred_probs, pred_index = extract_indexes(list(probs[0]))
-        st.subheader("Classification Status")
-        st.success(f"Waste Type: {CLASS_LABEL[pred_index[0]]}")
-        st.subheader("Contamination Status")
-        if (pred_probs[0] - pred_probs[1]) < CONTAMINATION_DIFFERENCE:
-            st.success(f"Contaminated: {CLASS_LABEL[pred_index[0]]} and {CLASS_LABEL[pred_index[1]]}")
-        else:
-            st.success("Not Contaminated")
+        #get the features
+        with st.spinner("Processing......."):
+            image_features = featurization(IMAGE_NAME, ConvNeXtXLarge_featurized_model)
+            probs = classification_model.predict_proba(image_features)
+            pred_probs, pred_index = extract_indexes(list(probs[0]))
+            st.subheader("Classification Status")
+            st.success(f"Waste Type: {CLASS_LABEL[pred_index[0]]}")
+            st.subheader("Contamination Status")
+            if (pred_probs[0] - pred_probs[1]) < CONTAMINATION_DIFFERENCE:
+                st.success(f"Contaminated: {CLASS_LABEL[pred_index[0]]} and {CLASS_LABEL[pred_index[1]]}")
+            else:
+                st.success("Not Contaminated")
         
 
 
